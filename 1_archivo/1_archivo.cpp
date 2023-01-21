fe6cf970c88847765a9cbd4fd7c642239d9cc05f
Marckycat <marco.villegas.lira@gmail.com>
2023-01-20 18:01:43 -0600

    Manejo de archivos y apuntadores.

--- 1_archivo/1_archivo.cpp
+++ 1_archivo/1_archivo.cpp
@@ -6,8 +6,59 @@
 #include <fstream>
 #define MAXSIZE 128
 
-int main()
+
+// 1_apuntadores.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+//pasar por referecnia
+//pasar por valor
+void acumular(float& num, float valor) {
+    num += valor;
+}
+
+
+void ejemploApuntadores() {
+    std::cout << "ejemplo de apuntadores\n";
+
+    int valor = 66;
+    int * ref2 = &valor; //Operador de referencia
+    *ref2 = 128;
+
+    printf("Valor: %i Mem:%#010x \n", *ref2, ref2);
+    
+    std::cout << "valor =" << valor << std::endl;
+}
+
+void promedio(float* valor_acum) {
+    *valor_acum += *valor_acum;
+}
+
+int main(int argc, char** argv)
 {
+    //si no hay argumentos, cerrar programa
+    if (argc <= 1) return 0;
+
+    float suma = 0;
+    std::cout << "argc: \n";
+    //argv[0] es el nombre del programa
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+        acumular(suma, atoi(argv[i]));
+    }
+
+    float promedio = suma / (argc - 1);
+    std::cout << "promedio: " << promedio << std::endl;
+
+    /*
+    int x = 18;
+    acumular(x, 8);
+    std::cout << "nuevo valor: "<< x << std::endl;
+    */
+    return 0;
+
+    int* v = 0;
+    return 0;
+
     //enviar la cadena HW al flujo std::cout
     std::cout << "Leyendo archivo" << std::endl;
 
@@ -22,6 +73,12 @@ int main()
         while (!file.eof()) {
             file.getline(buffer, MAXSIZE);
             std::cout << buffer << std::endl;
+
+            if (strcmp(buffer, "apuntadores") == 0)
+                ejemploApuntadores();
+
+            if (strcmp(buffer, "promedio") == 0)
+                ejemploApuntadores();
         }
     }
     else {
